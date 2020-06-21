3284c0583766e5e4dd3f2eececcaf9914e4bb858
hwipl <33433250+hwipl@users.noreply.github.com>
2020-06-21 15:15:54 +0200

    update console output
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- fwd.go
+++ fwd.go
@@ -63,6 +63,29 @@ func parse_command_line() {
 	}
 }
 
+// print_info prints an info/settings header to the console
+func print_info() {
+	sep := strings.Repeat("-", 70)
+	port := fmt.Sprintf("%d", dport)
+	if dport == 0 {
+		port = "any"
+	}
+
+	pFmt := "Receiving broadcast packets with destination port:    %s\n"
+	dFmt := "Forwarding packets to IP:                             %s\n"
+	sFmt := "Rewriting source address to IP:                       %s\n"
+
+	fmt.Println(sep)
+	fmt.Printf(pFmt, port)
+	for _, ip := range dests {
+		fmt.Printf(dFmt, ip)
+	}
+	if srcIP != nil {
+		fmt.Printf(sFmt, srcIP)
+	}
+	fmt.Println(sep)
+}
+
 // run_socket_loop runs the main socket loop, reading packets from the socket
 // and forwarding them to destination ip addresses
 func run_socket_loop() {
@@ -79,12 +102,7 @@ func run_socket_loop() {
 	}
 
 	// print some info before entering main loop
-	fmt.Printf("Receiving broadcast packets with destination port %d\n",
-		dport)
-	fmt.Printf("Forwarding received packets to IPs %s\n", dests)
-	if srcIP != nil {
-		fmt.Printf("Rewriting source address to %s\n", srcIP)
-	}
+	print_info()
 
 	// create packet buffer and start reading packets from raw socket
 	buf := make([]byte, 2048)
